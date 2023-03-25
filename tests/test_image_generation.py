a89af79557ae3391772800f6470418394c28daf8
Liam Moore <lmoore90@gmail.com>
2023-03-25 22:31:28 +0100

    fix tests to match endpoint api spec

--- tests/test_image_generation.py
+++ tests/test_image_generation.py
@@ -2,8 +2,19 @@ import pytest
 from tests.conftest import test_app
 from app.models.image import CharacterDescription
 
-@pytest.mark.parametrize("description", ["A brave knight", "A cunning thief"])
-def test_generate_image(description, test_app):
-    response = test_app.post("/api/v1/generate-image", json={"description": description})
+@pytest.mark.parametrize("description, name", 
+    [
+        ("A brave knight", "Sir Sweatsalot"),
+        ("A cunning thief", "Pokey")
+    ]
+)
+def test_generate_image(description, name, test_app):
+    response = test_app.post(
+        "/api/v1/generate-image",
+        json={
+            "description": description,
+            "name": name
+        }
+    )
     assert response.status_code == 200
     assert "image_url" in response.json()
